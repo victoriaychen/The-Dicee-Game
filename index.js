e81f0bf16ca028dcb4ffe7ecc28b66af6beedcca
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-01-24 17:12:52 -0800

    Updated index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -20,10 +18,8 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
+} else if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
+} else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
