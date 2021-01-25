208a13b0a97b1b30b4a98d59849d3f7caf6e5043
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-01-24 17:16:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
+}
+else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else {
+}
+else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
